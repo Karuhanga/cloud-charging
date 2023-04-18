3879b60fa422efc6269ae9af34f63a0c90872868
Lincoln Karuhanga <lincoln.karuhanga@gmail.com>
2023-04-18 17:56:16 +0300

    add memcached changes.

--- src/memcached.js
+++ src/memcached.js
@@ -1,65 +1,79 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+
 const memcached = require("memcached");
+
 const KEY = `account1/balance`;
 const DEFAULT_BALANCE = 100;
 const MAX_EXPIRATION = 60 * 60 * 24 * 30;
+
 const memcachedClient = new memcached(`${process.env.ENDPOINT}:${process.env.PORT}`);
+
 exports.resetMemcached = async function () {
     var ret = new Promise((resolve, reject) => {
         memcachedClient.set(KEY, DEFAULT_BALANCE, MAX_EXPIRATION, (res, error) => {
             if (error)
-                resolve(res);
+                reject(res);
             else
-                reject(DEFAULT_BALANCE);
+                resolve(DEFAULT_BALANCE);
         });
     });
     return ret;
 };
+
 exports.chargeRequestMemcached = async function (input) {
-    var remainingBalance = await getBalanceMemcached(KEY);
+    const remainingBalanceIdentifiable = await getBalanceMemcached(KEY);
+    let remainingBalance = Number(remainingBalanceIdentifiable[KEY]);
     const charges = getCharges();
     const isAuthorized = authorizeRequest(remainingBalance, charges);
-    if (!authorizeRequest(remainingBalance, charges)) {
+    if (!isAuthorized) {
         return {
             remainingBalance,
             isAuthorized,
             charges: 0,
         };
     }
-    remainingBalance = await chargeMemcached(KEY, charges);
+    remainingBalance = await chargeMemcached(KEY, charges, remainingBalanceIdentifiable);
     return {
         remainingBalance,
         charges,
         isAuthorized,
     };
 };
+
 function authorizeRequest(remainingBalance, charges) {
     return remainingBalance >= charges;
 }
+
 function getCharges() {
     return DEFAULT_BALANCE / 20;
 }
-async function getBalanceMemcached(key) {
+
+function getBalanceMemcached(key) {
     return new Promise((resolve, reject) => {
-        memcachedClient.get(key, (err, data) => {
+        memcachedClient.gets(key, (err, data) => {
             if (err) {
                 reject(err);
             }
             else {
-                resolve(Number(data));
+                resolve(data);
             }
         });
     });
 }
-async function chargeMemcached(key, charges) {
+
+function chargeMemcached(key, charges, remainingBalanceIdentifiable) {
+    let remainingBalance = Number(remainingBalanceIdentifiable[KEY]);
+    const newBalance = remainingBalance - charges;
+
     return new Promise((resolve, reject) => {
-        memcachedClient.decr(key, charges, (err, result) => {
+        memcachedClient.cas(
+            key, newBalance, remainingBalanceIdentifiable.cas, MAX_EXPIRATION, (err, result) => {
             if (err) {
                 reject(err);
             }
             else {
-                return resolve(Number(result));
+                return resolve(newBalance);
             }
         });
     });
